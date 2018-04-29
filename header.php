fd9b7f4a88202e3129c612569f5f9f84c500686e
architecturequeen <olawunmitaylor@gmail.com>
2018-04-29 20:02:16 +0100

    feat: add about section

---
+++ header.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+	<title>Charity</title>
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/vendor/css/base-min.css'; ?>">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/vendor/css/ionicons.min.css'; ?>">
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/vendor/css/grids-min.css'; ?>" >
+	<!--[if lte IE 8]>
+		<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/vendor/css/grids-responsive-old-ie-min.css'; ?>" >
+	<![endif]-->
+	<!--[if gt IE 8]><!-->
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri().'/vendor/css/grids-responsive-min.css'; ?>" >
+	<!--<![endif]-->
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="logo"> [ Logo Here ] </div>
+			<i class="ion-navicon mobile-menu"></i>
+			<ul class="main-nav">
+				<li>About</li>
+				<li>How it Works</li>
+				<li>Apply</li>
+				<li>Donate</li>
+				<li>Trustees</li>
+			</ul>
+		</nav>
+	</header>
+
+
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<section class="section-about">
+	<h2>About</h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	<div class="pure-g">
+		<div class="pure-u-1 pure-u-sm-1-2">
+			<i class="ion-ios-bookmarks-outline icon-big"></i>
+			<h3>Academic</h3>
+			<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi </p>
+		</div>
+		<div class="pure-u-1 pure-u-sm-1-2">
+			<i class="ion-ios-lightbulb-outline icon-big"></i>
+			<h3>Business</h3>
+			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+		</div>
+	</div>
+</section>
